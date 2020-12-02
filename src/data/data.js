f0659623e3c91ee269aa7efe744d27e273df3d62
Lukasz Grela <LGrela@omnicogroup.com>
2020-12-02 12:43:28 +0000

    static data

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,14 @@
-// src/data/data.js
-const notes = []
+const notes = [
+  { id: 1606901967247, title: "Note 0" },
+  { id: 1606901967248, title: "Note 1" },
+  { id: 1606901967249, title: "Note 2" },
+  { id: 1606901967250, title: "Note 3" },
+  { id: 1606901967251, title: "Note 4" },
+  { id: 1606901967252, title: "Note 5" },
+  { id: 1606901967253, title: "Note 6" },
+  { id: 1606901967254, title: "Note 7" },
+  { id: 1606901967255, title: "Note 8" },
+  { id: 1606901967256, title: "Note 9" },
+];
 
-module.exports = notes
+module.exports = notes;
