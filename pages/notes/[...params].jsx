2a7194bd9b0d53465ee47c3d3c2850efa001ae64
Lukasz Grela <LGrela@omnicogroup.com>
2020-12-02 12:49:45 +0000

    load note data

--- pages/notes/[...params].jsx
+++ pages/notes/[...params].jsx
@@ -4,15 +4,40 @@ import { jsx } from "theme-ui";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-const Note = () => {
+const Note = ({ note }) => {
   const router = useRouter();
   const { params } = router.query;
 
   return (
     <div sx={{ variant: "containers.page" }}>
-      <h1>Note: {params} </h1>
+      <h1>{note.title} </h1>
     </div>
   );
 };
 
 export default Note;
+
+export async function getServerSideProps({ params: p, req, res }) {
+  const { params } = p;
+  const url = `${process.env.API_URL}/api/note/${params[0]}`;
+  const notes = await fetch(url);
+
+  if (!notes.ok) {
+    res.writeHead(302, {
+      Location: "/notes",
+    });
+    res.end();
+    return {
+      props: {},
+    };
+  }
+  const { data } = await notes.json().catch((e) => {
+    console.error(e);
+    return Promise.resolve({});
+  });
+  return {
+    props: {
+      note: data,
+    },
+  };
+}
