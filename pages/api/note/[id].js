e5d2ce95f2e646eee1e9adf8f00026711120b4cc
Lukasz Grela <LGrela@omnicogroup.com>
2020-12-02 12:49:17 +0000

    update note api

--- pages/api/note/[id].js
+++ pages/api/note/[id].js
@@ -1,51 +1,53 @@
 // pages/api/note/[id].js
-import nc from 'next-connect'
-import notes from '../../../src/data/data'
+import nc from "next-connect";
+import notes from "../../../src/data/data";
 
-const getNote = id => notes.find(n => n.id === parseInt(id))
+const getNote = (id) => {
+  console.log("getNote", id, notes);
+  return notes.find((n) => `${n.id}` === `${id}`);
+};
 
 const handler = nc()
   .get((req, res) => {
-    
-    const note = getNote(req.query.id)
+    const note = getNote(req.query.id);
 
     if (!note) {
-      res.status(404)
-      res.end()
-      return
+      res.status(404);
+      res.json({ id: req.query.id });
+      res.end();
+      return;
     }
 
-    res.json({data: note})
+    res.json({ data: note });
   })
   .patch((req, res) => {
-    const note = getNote(req.query.id)
+    const note = getNote(req.query.id);
 
     if (!note) {
-      res.status(404)
-      res.end()
-      return
+      res.status(404);
+      res.end();
+      return;
     }
-    
-    const i = notes.findIndex(n => n.id === parseInt(req.query.id))
-    const updated = {...note, ...req.body}
 
-    notes[i] = updated
-    res.json({data: updated})
+    const i = notes.findIndex((n) => n.id === parseInt(req.query.id));
+    const updated = { ...note, ...req.body };
+
+    notes[i] = updated;
+    res.json({ data: updated });
   })
   .delete((req, res) => {
-    const note = getNote(req.query.id)
+    const note = getNote(req.query.id);
 
     if (!note) {
-      res.status(404)
-      res.end()
-      return
+      res.status(404);
+      res.end();
+      return;
     }
-    const i = notes.findIndex(n => n.id === parseInt(req.query.id))
-    
-    notes.splice(i, 1)
+    const i = notes.findIndex((n) => n.id === parseInt(req.query.id));
 
-    res.json({data: req.query.id})
-  })
-  
+    notes.splice(i, 1);
 
-export default handler
+    res.json({ data: req.query.id });
+  });
+
+export default handler;
